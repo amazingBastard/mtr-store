82e8ab69fc1693909d340616dc59164524b82f32
Damir <damir@withpulp.com>
2015-09-27 23:49:32 -0400

    remove cart item event

--- app/client/templates/modules/cart/cart_buttons.js
+++ app/client/templates/modules/cart/cart_buttons.js
@@ -1,11 +1,20 @@
 Template.toggleCart.events({
-   'click .toggle.cart.button': function() {
+   'click .toggle.cart.button': function(event) {
+       event.preventDefault();
        Session.set('activeCart', !Session.get('activeCart'));
    }
 });
 
 Template.insertCartItem.events({
-    'click .insert.item.button': function() {
+    'click .insert.item.button': function(event) {
+        event.preventDefault();
         console.log('insert item to cart items collection');
     }
 });
+
+Template.removeCartItem.events({
+    'click .remove.item.button': function(event) {
+        event.preventDefault();
+        console.log('remove item from cart collection');
+    }
+})
