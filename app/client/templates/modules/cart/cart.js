b7275bb4ad3b7c313b0591f12a1563d50611a649
Damir <damir@withpulp.com>
2015-09-28 05:12:36 -0400

    updated styles and cart helpers

--- app/client/templates/modules/cart/cart.js
+++ app/client/templates/modules/cart/cart.js
@@ -9,22 +9,22 @@ Template.cart.onRendered(function () {
     Session.setDefault('activeCart', false);
     Session.setDefault('itemCount', 0);
     Session.setDefault('itemTotal', 0);
+
+    Tracker.autorun(function () {
+        var total = 0,
+            items = Items.find({}, {fields: {price: 1}});
+
+        items.forEach(function(item){
+            total += item.price;
+        });
+
+        Session.set('itemTotal', Math.floor(total*100)/100);
+        Session.set('itemCount', items.count());
+    });
 });
 
 Template.cart.helpers({
     items: function () {
         return Items.find({}, {sort: {created: -1}});
     }
-});
-
-Tracker.autorun(function () {
-    //var total = 0,
-    //    items = Items.find({}, {fields: {price: 1}});
-    //
-    //items.forEach(function(item){
-    //    total += item.price;
-    //});
-    //
-    //Session.set('itemTotal', Math.floor(total*100)/100);
-    //Session.set('itemCount', items.count());
 });
