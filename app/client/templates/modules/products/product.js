7bd74a17b13f16d794b524cca3df832ecbfe3d64
Damir <damir@withpulp.com>
2015-09-29 19:41:43 -0400

    added condition to set qty to 1 if no qty is entered

--- app/client/templates/modules/products/product.js
+++ app/client/templates/modules/products/product.js
@@ -2,9 +2,12 @@ Template.product.events({
     'click .insert.item.button': function (event, template) {
         event.preventDefault();
 
-        var qty = template.find('.qty.input').value,
-            product = this._id;
+        var product = this._id,
+            qty = template.find('.qty.input').value;
 
+        if (qty === '') {
+            qty = '1';
+        }
 
         Meteor.call('insertItem', qty, product, function (error) {
             if (error) {
