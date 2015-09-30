aa3ef4afdd3e7c480a1beead3f07b17a749ff76b
Damir <damir@withpulp.com>
2015-09-30 18:15:16 -0400

    added checkout view and redirect template - wip

--- app/router/routes.js
+++ app/router/routes.js
@@ -8,10 +8,21 @@ FlowRouter.route('/', {
     }
 });
 
+FlowRouter.route('/checkout', {
+    name: 'checkout',
+    action: function () {
+        BlazeLayout.render('layout', {
+            content: 'checkout'
+        });
+        //GAnalytics.pageview();
+    }
+});
+
 FlowRouter.notFound = {
     action: function() {
         BlazeLayout.render('layout', {
             content: 'redirect'
         });
+        //GAnalytics.pageview();
     }
 };
